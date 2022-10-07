268c059d1e9da7d2d26b919d46c89117f73586b4
Daguerre45 <alberto.daguerre02@gmail.com>
2022-10-07 13:29:49 +0200

    COPIADO DE MARIANO

---
+++ tetris.pl
@@ -0,0 +1,29 @@
+lanzarPieza(Columna):-
+    contador(columna,N),
+    retractall(contador(columna,N)),
+    N4 is N + 4,
+    assert(contador(columna,N4)),
+
+imborrable:-
+    contador(columna,N), N =:= 0,
+
+borrarFila:-
+    \+imborrable,
+    decrementarTodos.
+
+decrementarTodos:-
+    decrementar(10).
+
+decrementarTodos(0).
+decrementarTodos(Columna):-
+    columna > 0,
+    decrementar(Columna),
+    Columna1 is Columna - 1,
+    decrementarTodos(Columna1).
+
+decrementar(Columna):-
+    contador(Columna,N),
+    retractall(contador(Columna,N)),
+    N1 is N - 1,
+    assert(contador(Columna,N1)).
+    
