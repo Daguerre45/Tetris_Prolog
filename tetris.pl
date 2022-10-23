950b4562cfa05015c6cf51863ab6548ffed6d8b4
Daguerre45 <alberto.daguerre02@gmail.com>
2022-10-23 13:09:51 +0200

    cambios

--- tetris.pl
+++ tetris.pl
@@ -3,6 +3,8 @@
 :- dynamic contador/2.
 
 %en la columna x lanzar pieza
+lanzarPieza(Columna):-
+    contador(Columna,N), %N es el numero de huecos que hay ocupados en la columna
 lanzarPiezaVertical(Columna):-
     contador(Columna,N), %N es el numero de huecos que hay ocupados en la columna EN REALIDAD ES (X,Y)
     retractall(contador(Columna,N)),
@@ -24,33 +26,50 @@ lanzarPiezaCuadrado(Columna):-
     assert(contador(Columna,N2)),
     assert(contador(Columna2,N3)).
 
+lanzarPiezaLDerecha(Columna):-
+    \+ Columna = 10, %si la columna es 10 no se puede lanzar la pieza cuadrada
+    Columna2 is Columna + 1,
+    contador(Columna,N), %N es el numero de huecos que hay ocupados en la columna EN REALIDAD ES (X,Y)
+    contador(Columna2,N1),
+    retractall(contador(Columna,N)), %parte vertical de la L
+    retractall(contador(Columna2,N1)), %parte horizontal de la L
+    N2 is N + 3, %le sumo 4 ya que meto la pieza vertical
+    N3 is N1 + 1,
+    N2 =< 16, %si no se sale de la cuadricula
+    N3 =< 16, %si no se sale de la cuadricula
+    assert(contador(Columna,N2)),
+    assert(contador(Columna2,N3)).
 
-lanzamientoValidoCuadrado(Cuadrado,Columna, Fila):-
-    Columna1 is Columna + 1,
-    alturaMaxima(Columna, Columna1, Fila),
-    assert(casillaOcupada(Columna,Fila)),
-    assert(casillaOcupada(Columna1,Fila)),
-    Fila1 is Fila +1,
-    assert(casillaOcupada(Columna,Fila1)),
-    assert(casillaOcupada(Columna1,Fila1)).
+lanzarPiezaLIzquierda(Columna):-
+    \+ Columna = 1, %si la columna es 10 no se puede lanzar la pieza cuadrada
+    Columna2 is Columna - 1,
+    contador(Columna,N), %N es el numero de huecos que hay ocupados en la columna EN REALIDAD ES (X,Y)
+    contador(Columna2,N1),
+    retractall(contador(Columna,N)), %parte vertical de la L
+    retractall(contador(Columna2,N1)), %parte horizontal de la L
+    N2 is N + 3, %le sumo 4 ya que meto la pieza vertical
+    N3 is N1 + 1,
+    N2 =< 16, %si no se sale de la cuadricula
+    N3 =< 16, %si no se sale de la cuadricula
+    assert(contador(Columna,N2)),
+    assert(contador(Columna2,N3)).
 
 imborrable:-
     contador(columna,N), N =:= 0. %N es igual a 0
+
 
 borrarFila:-
     \+imborrable,
     decrementarTodos.
 
+
 decrementarTodos:-
     decrementar(10).
 
-
-decrementarTodos(0). % Caso base
-decrementarTodos(Columna):-
-    columna > 0,
-    decrementar(Columna),
+@@ -27,11 +70,20 @@
     Columna1 is Columna - 1,
     decrementarTodos(Columna1).
+
 
 decrementar(Columna):-
     contador(Columna,N),
